fa905d28cd7fed8a76ce457738c0d2dc6dc1dbff
Affan <affantaufiqur@gmail.com>
2023-12-10 22:28:34 +0700

    add category

---
+++ category.js
@@ -0,0 +1,74 @@
+/**
+ * Represents a category
+ * @class
+ */
+
+export class Category {
+	/**
+	 * Type definition for product
+	 * @typedef {object} productsData - product object
+	 * @property {number} productId
+	 * @property {string} productName
+	 * @property {number} price
+	 * @property {string} description
+	 */
+
+	/**
+	 * @param {number} categoryId - category ID
+	 * @param {string} categoryName - category name
+	 */
+	constructor(categoryId, categoryName) {
+		this.categoryId = categoryId;
+		this.categoryName = categoryName;
+		/**
+		 * An array of products
+		 * @type {productsData[]} productsData
+		 */
+		this.productsData = [];
+	}
+
+	/**
+	 * Displaying information about the product
+	 * @returns {string} - The information of the category
+	 */
+	displayInfo() {
+		return `Category: ${this.categoryName}, id: ${this.categoryId}`;
+	}
+
+	/**
+	 * Update the category
+	 * @returns {object} - Return the categoryName and categoryId
+	 */
+	updateCategory() {
+		return this;
+	}
+
+	/**
+	 * Type definition for product args
+	 * @typedef {object} productObject
+	 * @property {number} productId
+	 * @property {string} productName
+	 * @property {number} price
+	 * @property {string} description
+	 */
+
+	/**
+	 * Add products
+	 * @param {productObject} product
+	 */
+
+	products(product) {
+		// check if product.productId is already exist
+		const productExist = this.productsData.find((p) => p.productId === product.productId);
+		if (productExist) {
+			throw new Error(`Product with ID ${product.productId} already exist.`);
+		}
+		this.productsData.push({
+			productId: product.productId,
+			productName: product.productName,
+			price: product.price,
+			description: product.description,
+		});
+		return this.productsData;
+	}
+}
